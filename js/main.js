2315e957b202e84e1ddc282475ac221bef1b9906
neela11 <neelambika11@gmail.com>
2015-11-28 17:55:56 -0800

    condition for decimal number

--- js/main.js
+++ js/main.js
@@ -2,29 +2,35 @@
 function display(){
 var num = document.getElementById("x").value;
 
-var n=parseInt(num);
+//var n=parseInt(num);
+if(num%1 !=0){
+  document.write("enter a whole number");
 
-fizzbuzz(n);
 }
-function fizzbuzz(n){
+else{
+fizzbuzz(num);
+}
+}
+function fizzbuzz(num){
 
 var i=0;
 
-for(i=1;i<n;i++){
-     if((i%5)==0 && (i%3)==0){
-        document.write("fizzbuzz"+"<br>");
-      }
-      else if((i%5)==0){
-        document.write("buzz"+"<br>")
-       }
-     else if((i%3)==0){
-        document.write("fizz"+"<br>")
-       }
-      else{
-        document.write(i+"<br>");
-       }
-}
+
+  for(i=1;i<num;i++){
+       if((i%5)==0 && (i%3)==0){
+          document.write("fizzbuzz"+"<br>");
+        }
+        else if((i%5)==0){
+          document.write("buzz"+"<br>")
+         }
+       else if((i%3)==0){
+          document.write("fizz"+"<br>")
+         }
+        else{
+          document.write(i+"<br>");
+         }
+    }
+
+  
 
 }
-
-
